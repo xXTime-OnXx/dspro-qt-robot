293ca17fa4972f902af85c04cfdd6696b3a3c222
FabianDubach <fabian.dubach@outlook.com>
2024-12-06 09:38:59 +0100

    added imports for i_spy

--- QTrobot_dev/qt_robot/src/i_spy.py
+++ QTrobot_dev/qt_robot/src/i_spy.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python3
+import rospy
+import cv2
+import time
+import random
+
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+
+from qt_robot_interface.srv import behavior_talk_text
+from qt_gesture_controller.srv import gesture_play
+from custom_interfaces.srv import MicrophoneBasedSpeechRecognition
+from custom_interfaces.srv import Detectron
+from cv_bridge import CvBridge
+
 class ISpy():
     
     def __init__():
